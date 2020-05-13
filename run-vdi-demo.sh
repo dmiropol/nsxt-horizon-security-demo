8f4535f94102f2076295b95dfeb2b8de1bcb42d6
dmiropol <16676640+dmiropol@users.noreply.github.com>
2020-05-13 10:32:02 -0400

    changed for sdfwapi.py

--- run-vdi-demo.sh
+++ run-vdi-demo.sh
@@ -5,13 +5,21 @@ if [ -z "$1" ]; then
 	exit 1
 else
   if [ "$1" == 'create' ]; then
-    python dfwapi.py groups.json "$1"
-    python dfwapi.py services.json "$1"
-    python dfwapi.py policies.json "$1"
+    python sdfwapi.py groups.json "$1"
+    python sdfwapi.py services.json "$1"
+    python sdfwapi.py dfw_policies1.json "$1"
+    python sdfwapi.py dfw_policies2.json "$1"
+    python sdfwapi.py dfw_policies3.json "$1"
+    python sdfwapi.py ids_profiles.json "$1"
+    python sdfwapi.py ids_rules.json "$1"
   else
-    python dfwapi.py policies.json "$1"
-    python dfwapi.py services.json "$1"
-    python dfwapi.py groups.json "$1"
+    python sdfwapi.py dfw_policies1.json "$1"
+    python sdfwapi.py dfw_policies2.json "$1"
+    python sdfwapi.py dfw_policies3.json "$1"
+    python sdfwapi.py ids_profiles.json "$1"
+    python sdfwapi.py ids_rules.json "$1"
+    python sdfwapi.py services.json "$1"
+    python sdfwapi.py groups.json "$1"
   fi
 fi
 echo 'All done'
