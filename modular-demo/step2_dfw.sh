16521aaa739ea9a059376e76f9e38194df34efb5
dmiropol <16676640+dmiropol@users.noreply.github.com>
2020-05-22 16:10:53 -0400

    updated with AD-based policies
    for onecloud pod with horizon + intelligence

--- modular-demo/step2_dfw.sh
+++ modular-demo/step2_dfw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ../
-python sdfwapi.py dfw_policies2.json create
-sleep 1
-echo 'If no errors in the output above - VDI environment and Apps are now secured by NSX!'
+python sdfwapi.py dfw_policies2_vdionly.json create
+python sdfwapi.py dfw_policies2_appsonly_AD.json create
+#python sdfwapi.py dfw_policies2_appsonly_IC.json create
+
+echo 'All done!'
